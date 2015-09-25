123e6f128e752387dba727986d3dd82fee423afe
rogb29 <rogb29@hotmail.com>
2015-09-25 10:05:01 -0600

    cambio ro

--- ProyectoInge/ProyectoInge/About.aspx.cs
+++ ProyectoInge/ProyectoInge/About.aspx.cs
@@ -9,7 +9,7 @@ namespace ProyectoInge
 {
     public partial class About : Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        protected void Page_Load(object sender,EventArgs e)
         {
 
         }
