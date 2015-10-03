ed1d9a10f89d59aacbba4ba4ca7f346c5386f42e
Leandro09 <LuisLeandro09@gmail.com>
2015-10-03 15:27:58 -0600

    Cambios en recursos humanos

---
+++ ProyectoInge/ProyectoInge/App_Code/Capa de Acceso a Datos/ControladoraBDRecursos.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using DataSetVentaTableAdapters;
+using System.Data.SqlClient;
+
+namespace ProyectoInge.App_Code.Capa_de_Acceso_a_Datos
+{
+    public class ControladoraBDRecursos : Controller
+    {
+        // GET: ControladoraBDRecursos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ControladoraBDRecursos/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ControladoraBDRecursos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ControladoraBDRecursos/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ControladoraBDRecursos/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ControladoraBDRecursos/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ControladoraBDRecursos/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ControladoraBDRecursos/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ProyectoInge/ProyectoInge/App_Code/Capa de Control/ControladoraRecursos.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+
+namespace ProyectoInge.App_Code.Capa_de_Control
+{
+    public class ControladoraRecursos 
+    {
+        // GET: ControladoraRecursos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ControladoraRecursos/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ControladoraRecursos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ControladoraRecursos/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ControladoraRecursos/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ControladoraRecursos/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ControladoraRecursos/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ControladoraRecursos/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
