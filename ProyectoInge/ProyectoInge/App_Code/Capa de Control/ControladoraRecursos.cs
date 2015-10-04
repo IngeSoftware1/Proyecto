1cbcc301e0a06f35dc8b42cd74e8b1de6bba79a5
Gaudy <gbm2494@hotmail.com>
2015-10-04 15:06:53 -0600

    Agregar todas las clases Entidad

--- ProyectoInge/ProyectoInge/App_Code/Capa de Control/ControladoraRecursos.cs
+++ ProyectoInge/ProyectoInge/App_Code/Capa de Control/ControladoraRecursos.cs
@@ -7,98 +7,126 @@ using System.Data;
 
 namespace ProyectoInge.App_Code.Capa_de_Control
 {
-  /*  public class ControladoraRecursos 
-    {
-        ControladoraBDRecursos controladoraBDRecursos = new ControladoraBDRecursos();
+    /*  public class ControladoraRecursos 
+      {
+          ControladoraBDRecursos controladoraBDRecursos = new ControladoraBDRecursos();
 
 
-        public ControladoraRecursos()
-	    {
+          public ControladoraRecursos()
+          {
 
-	    }
+          }
 
+         //Método para llenar obtener todos los recursos humanos del sistema
+         //Requiere: Requiere la cedula del usuario actual
+         //Modifica: Obtiene el datable de todos los recursos humanos del sistema
+         //Retorna: Un datatable
+         public DataTable consultarRecursosHumanos(string cedula)
+          {
+              return controladoraBDRecursos.consultarRH(cedula);
+          }
+     
+     
+     //Método para determinar la acción que se esta realizando en la interfa
+      public void ejecutarAccion(int modo, Object [] datos) {
+        switch (modo) {
+            case 1: { // INSERTAR
+                Funcionario funcionario = new Funcionario(datos);
+                controladoraBDVenta.insertarVenta(venta);
+                
+            };
+            break;
+            case 2:
+            { // MODIFICAR
+                EntidadVenta venta = new EntidadVenta(datos);
+                controladoraBDVenta.modificarVenta(venta);
 
-        public DataTable consultarLoginRecursosHumanos(string user, string password)
-        {
-            return controladoraBDRecursos.consultarVenta(user, password);
+            };
+            break;
         }
+    }
+     * 
+          public DataTable consultarLoginRecursosHumanos(string user, string password)
+          {
+              return controladoraBDRecursos.consultarVenta(user, password);
+          }
 
-        // GET: ControladoraRecursos
-        public ActionResult Index()
-        {
-            return View();
-        }
+          // GET: ControladoraRecursos
+          public ActionResult Index()
+          {
+              return View();
+          }
 
-        // GET: ControladoraRecursos/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
+          // GET: ControladoraRecursos/Details/5
+          public ActionResult Details(int id)
+          {
+              return View();
+          }
 
-        // GET: ControladoraRecursos/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+          // GET: ControladoraRecursos/Create
+          public ActionResult Create()
+          {
+              return View();
+          }
 
-        // POST: ControladoraRecursos/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
+          // POST: ControladoraRecursos/Create
+          [HttpPost]
+          public ActionResult Create(FormCollection collection)
+          {
+              try
+              {
+                  // TODO: Add insert logic here
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+                  return RedirectToAction("Index");
+              }
+              catch
+              {
+                  return View();
+              }
+          }
 
-        // GET: ControladoraRecursos/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
+          // GET: ControladoraRecursos/Edit/5
+          public ActionResult Edit(int id)
+          {
+              return View();
+          }
 
-        // POST: ControladoraRecursos/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
+          // POST: ControladoraRecursos/Edit/5
+          [HttpPost]
+          public ActionResult Edit(int id, FormCollection collection)
+          {
+              try
+              {
+                  // TODO: Add update logic here
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+                  return RedirectToAction("Index");
+              }
+              catch
+              {
+                  return View();
+              }
+          }
 
-        // GET: ControladoraRecursos/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+          // GET: ControladoraRecursos/Delete/5
+          public ActionResult Delete(int id)
+          {
+              return View();
+          }
 
-        // POST: ControladoraRecursos/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
+          // POST: ControladoraRecursos/Delete/5
+          [HttpPost]
+          public ActionResult Delete(int id, FormCollection collection)
+          {
+              try
+              {
+                  // TODO: Add delete logic here
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-    }*/
+                  return RedirectToAction("Index");
+              }
+              catch
+              {
+                  return View();
+              }
+          }
+      }*/
 }
---
+++ ProyectoInge/ProyectoInge/App_Code/Capa de Datos (Entidad)/EntidadRequerimiento.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoInge.App_Code.Capa_de_Datos__Entidad_
+{
+    public class EntidadRequerimiento
+    {
+    }
+}
