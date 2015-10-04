9eea1354974ada1a7de7ca5ec70b29fc5d01a171
Gaudy <gbm2494@hotmail.com>
2015-10-04 15:26:28 -0600

    Ultimos cambios controladora BD recursos humanos
    Hay que modificar los métodos porq solo están copiados del ejemplo
    ventas

--- ProyectoInge/ProyectoInge/App_Code/Capa de Acceso a Datos/ControladoraBDRecursos.cs
+++ ProyectoInge/ProyectoInge/App_Code/Capa de Acceso a Datos/ControladoraBDRecursos.cs
@@ -8,84 +8,156 @@ using System.Data.SqlClient;
 
 namespace ProyectoInge.App_Code.Capa_de_Acceso_a_Datos
 {
-  /*  public class ControladoraBDRecursos
+    /*  public class ControladoraBDRecursos
+      {
+     
+         //RHTableAdapter adapterRH;                  Hay que revisar como crearlo
+      
+     public ControladoraBDRecursos()
+      {
+          //adapterRH = new RHTableAdapter();	
+      }
+
+     public DataTable consultarRH(string cedula) {
+        DataTable dt = new DataTable();
+        //dt = adapterRH.GetData(cedula);
+        return dt;
+    }
+     
+     /////De aquí hacía abajo hay que modificarlo para funcionarios porque lo copie de ventas
+     public void insertarVenta(EntidadRH recurso) {
+        try
+        {
+            //Hay que modificarlo para funcionario
+            //this.adapterRH.Insert(venta.ID, venta.Fecha, venta.Proveedor, venta.Descripcion, venta.NombreProducto, venta.CantidadInventario, venta.CantidadSolicitada); 
+        }
+        catch (SqlException e)
+        {
+            int r = e.Number;
+            if (r == 2627)
+            {
+                // "Ya existe una venta con este id";
+            }
+            else
+            {
+                //"Se ha producido un error al insertar la venta";
+            }
+        }
+    }
+
+    public void modificarVenta(EntidadVenta venta)
     {
-        // GET: ControladoraBDRecursos
-        public ActionResult Index()
+        try
         {
-            return View();
+            //this.adapterVentas.Update(venta.ID, venta.Fecha, venta.Proveedor, venta.Descripcion, venta.NombreProducto, venta.CantidadInventario, venta.CantidadSolicitada, venta.ID); 
         }
-
-        // GET: ControladoraBDRecursos/Details/5
-        public ActionResult Details(int id)
+        catch (SqlException e)
         {
-            return View();
-        }
-
-        // GET: ControladoraBDRecursos/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: ControladoraBDRecursos/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
+            int r = e.Number;
+            if (r == 2627)
             {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
+                // "Ya existe una venta con este id";
             }
-            catch
+            else
             {
-                return View();
+                //"Se ha producido un error al modificar la venta";
             }
         }
+    }
 
-        // GET: ControladoraBDRecursos/Edit/5
-        public ActionResult Edit(int id)
+    public void eliminarCuenta(int idVenta) {
+        try
         {
-            return View();
-        }
+            //adapterVentas.Delete(idVenta);
 
-        // POST: ControladoraBDRecursos/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        }
+        catch (SqlException e)
         {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            //"Ha ocurrido un error al eliminar la venta";
         }
+    }
 
-        // GET: ControladoraBDRecursos/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+    public DataTable consultarVenta(int idVenta) {
+        DataTable dt = new DataTable();
+        //dt = this.adapterVentas.consultarFila(idVenta);
+        return dt;
+    }
+          // GET: ControladoraBDRecursos
+          public ActionResult Index()
+          {
+              return View();
+          }
 
-        // POST: ControladoraBDRecursos/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
+          // GET: ControladoraBDRecursos/Details/5
+          public ActionResult Details(int id)
+          {
+              return View();
+          }
 
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-    }*/
+          // GET: ControladoraBDRecursos/Create
+          public ActionResult Create()
+          {
+              return View();
+          }
+
+          // POST: ControladoraBDRecursos/Create
+          [HttpPost]
+          public ActionResult Create(FormCollection collection)
+          {
+              try
+              {
+                  // TODO: Add insert logic here
+
+                  return RedirectToAction("Index");
+              }
+              catch
+              {
+                  return View();
+              }
+          }
+
+          // GET: ControladoraBDRecursos/Edit/5
+          public ActionResult Edit(int id)
+          {
+              return View();
+          }
+
+          // POST: ControladoraBDRecursos/Edit/5
+          [HttpPost]
+          public ActionResult Edit(int id, FormCollection collection)
+          {
+              try
+              {
+                  // TODO: Add update logic here
+
+                  return RedirectToAction("Index");
+              }
+              catch
+              {
+                  return View();
+              }
+          }
+
+          // GET: ControladoraBDRecursos/Delete/5
+          public ActionResult Delete(int id)
+          {
+              return View();
+          }
+
+          // POST: ControladoraBDRecursos/Delete/5
+          [HttpPost]
+          public ActionResult Delete(int id, FormCollection collection)
+          {
+              try
+              {
+                  // TODO: Add delete logic here
+
+                  return RedirectToAction("Index");
+              }
+              catch
+              {
+                  return View();
+              }
+          }
+      }*/
 }
