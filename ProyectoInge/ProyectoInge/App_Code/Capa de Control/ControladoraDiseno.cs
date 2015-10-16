568c058b123884a2427b5ae4b6b2c0740a7642bd
rogb29 <rogb29@hotmail.com>
2015-10-16 10:39:30 -0600

    COrreccion error en controladora diseno

--- ProyectoInge/ProyectoInge/App_Code/Capa de Control/ControladoraDiseno.cs
+++ ProyectoInge/ProyectoInge/App_Code/Capa de Control/ControladoraDiseno.cs
@@ -9,11 +9,16 @@ namespace ProyectoInge.App_Code.Capa_de_Control
 {
     public class ControladoraDiseno
     {
-        ControladoraBDProyectos controladoraBDProyecto = new ControladoraBDProyectos();
+        ControladoraBDDiseno controladoraBDDiseno = new ControladoraBDDiseno();
 
+        /*Método para buscar, yendo a la base de datos, si existe un usuario con una cedula particular, que sea líder de diseño
+        * Requiere: un string con la cedula de un funcionario específico
+        * Modifica: Genera la consulta para mandarla a la base de datos para saber si hay un usuario líder de diseño.
+        * Retorna: true si se encontro que el funcionario tenía era líder y false si no.
+        */
         public bool buscarAsignacionMiembrosDiseno(string cedula)
         {
-            return controladoraBDProyecto.buscarAsignacionProyectos(cedula);
+            return controladoraBDDiseno.buscarAsignacionMiembrosDiseno(cedula);
         }
     }
 }
